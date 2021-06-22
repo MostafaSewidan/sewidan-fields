57cb5b962b0ef8dedbb3ed2b42034c42c643cf4b
MostafaSewidan <sewidanmostafa@gmail.com>
2021-06-22 15:59:03 +0200

    public v4

--- src/resources/views/fields/file.blade.php
+++ src/resources/views/fields/file.blade.php
@@ -1,5 +1,4 @@
 
-<link rel="stylesheet" href="{{asset('SewidanField/plugins/bootstrap-fileinput/css/fileinput.min.css')}}">
 {!! Form::file($name, $field_attributes) !!}
 
 <span class="holder" style="margin-top:15px;max-height:100px;">
@@ -7,7 +6,3 @@
         <img src="{{$value}}" style="height: 15rem;">
     @endif
 </span>
-
-
-<script src="{{asset('SewidanField/plugins/bootstrap-fileinput/js/fileinput.min.js')}}"></script>
-<script src="{{asset('SewidanField/plugins/bootstrap-fileinput/js/fileinput_locale_ar.js')}}"></script>

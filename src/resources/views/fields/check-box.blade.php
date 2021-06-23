b06f3a3ef025f737a5dc536337e8cd8471de1da0
MostafaSewidan <sewidanmostafa@gmail.com>
2021-06-23 16:31:04 +0200

    stable

--- src/resources/views/fields/check-box.blade.php
+++ src/resources/views/fields/check-box.blade.php
@@ -1,1 +1,3 @@
-{!! Form::checkbox($name, $label, $value , $field_attributes) !!}
+{!! Form::checkbox($name, $value ,
+(isset($field_attributes['checked']) && $field_attributes['checked'] == 'checked') ?  true : null ,
+$field_attributes) !!}

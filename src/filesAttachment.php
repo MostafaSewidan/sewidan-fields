ed5bb434fbecd067603d7b0ebeacf076bdf8cabe
MostafaSewidan <sewidanmostafa@gmail.com>
2022-01-23 11:54:48 +0200

    try ck editor 5 v5

---
+++ src/filesAttachment.php
@@ -0,0 +1,55 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\File;
+/**
+ * @param $file
+ * @param $folder_name
+ * @return string
+ */
+function addAttachment($file, $folder_name)
+{
+
+    ///////////////////////////////
+
+    $destinationPath = public_path().'/uploads/'.$folder_name.'/';
+    $extension = $file->getClientOriginalExtension(); // getting image extension
+
+    if (!File::isDirectory($destinationPath)) {
+        File::makeDirectory($destinationPath, 0755, true, true);
+    }
+
+    $image = $save.'-'.time().''.rand(11111, 99999).'.'.$extension;
+    $file->move($destinationPath, $image); // uploading file to given
+    $path = 'uploads/'.$folder_name.'/'.$image;
+
+    return $path;
+}
+
+/**
+ * @param $file
+ * @param $oldFilesPath
+ * @param $folder_name
+ * @return string
+ */
+function updateAttachment(
+    $file,
+    $oldFilesPath,
+    $folder_name
+) {
+    deleteAttachment($oldFilesPath);
+    addAttachment($file, $folder_name);
+}
+
+/**
+ * @param $file_path
+ * @return bool
+ */
+function deleteAttachment($file_path)
+{
+    foreach ((array) $file_path as $file) {
+
+        File::delete(public_path().'/'.$file);
+    }
+    return true;
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 Route::post('ckeditor/upload',function (\Illuminate\Http\Request $request){
-     $path = SewidanField\App\Traits\Attachment\Attachment::addAttachment($request->image,'ck-editor-images');
+     $path = addAttachment($request->image,'ck-editor-images');
     return response()->json(['url' => asset($path)]);
 })->name('ckeditor.upload');

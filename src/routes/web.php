8d91e815d7b1aee05754c540a46b92646977623b
MostafaSewidan <sewidanmostafa@gmail.com>
2022-10-22 16:00:56 +0200

    ck_editor_images_uploader in config

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 Route::post('ckeditor/upload',function (\Illuminate\Http\Request $request){
-     $path = addAttachment($request->image,'ck-editor-images');
-    return response()->json(['url' => asset($path)]);
+    if(config('field.ck_editor_images_uploader',false)){
+
+        $class = config('field.ck_editor_images_uploader');
+
+        return $class::upload($request);
+    }else{
+
+        $path = addAttachment($request->image,'ck-editor-images');
+        return response()->json(['url' => asset($path)]);
+    }
 })->name('ckeditor.upload')->middleware((array)config('field.ckeditor5.uploading_middleware'));

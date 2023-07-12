f2d03cc5650573a7fcae71c969b9f34a8458672b
Mostafa Sewidan <35866891+MostafaSewidan@users.noreply.github.com>
2023-07-12 14:56:20 +0300

    Update file.blade.php

--- src/resources/views/fields/file.blade.php
+++ src/resources/views/fields/file.blade.php
@@ -1,17 +1,7 @@
-
 {!! Form::file($name, $field_attributes) !!}
 
 <span class="holder" style="margin-top:15px;max-height:100px;">
     @if($value)
-        @php
-            $file = explode(".", $document->car_document);
-            $extenstion = end($file);
-        @endphp
-        @if(in_array($extenstion, config('field.file_extenstions', ["pdf"] )))
-            <a target="_blank" download="{{$name . '.'. $extenstion}}" href="{{$value}}">Download !</a>
-        @else
-            <img src="{{$value}}" style="height: 15rem;">
-        @endif
-      
+        <img src="{{$value}}" style="height: 15rem;">
     @endif
 </span>
